0825f378dce7b210450f9343bb9ed782d3d62f95
Jonathan McGaha <87310106+BsMWgEr@users.noreply.github.com>
2022-04-24 17:17:28 -0400

    Adding settings

--- stockvectorrigs/settings.py
+++ stockvectorrigs/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
+import os
 import django_heroku
 from pathlib import Path
 
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3yyd67iavw9=)x#@+6t8rt1b!(m@utl-b_hn0^j+ftoic^++u4'
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ["DEBUG"]
 
 ALLOWED_HOSTS = []
 
