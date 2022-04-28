6faa51cc6bfd4217956ef8df87a2a19bf3dad23b
Jonathan McGaha <87310106+BsMWgEr@users.noreply.github.com>
2022-04-28 01:18:04 -0400

    Adding sold data submit to inventory pages

--- api/views.py
+++ api/views.py
@@ -260,14 +260,15 @@ def sold_data_api(request):
     print(request.POST.get('inventory_item'))
     x = request.POST.get('inventory_item')
     print(form.data)
-    form.save()
-    form = SoldDataForm()
+    if form.is_valid():
+        form.save()
+        form = SoldDataForm()
     # after creating new sold data --> link to object via id
-    objs = InventoryObject.objects.all().filter(inventory_item_id=x)
-    last_sold = SoldDetail.objects.last()
-    objs.update(sold_data_id=last_sold.id)
-    print(objs)
-    print(last_sold)
+        objs = InventoryObject.objects.all().filter(inventory_item_id=x)
+        last_sold = SoldDetail.objects.last()
+        objs.update(sold_data_id=last_sold.id)
+        print(objs)
+        print(last_sold)
 
     context = {
         'form': form,
