c349cd895907e4185861f110ed708768535537b6
Jonathan McGaha <87310106+BsMWgEr@users.noreply.github.com>
2022-05-02 16:22:54 -0400

    adding upload functions to create form

--- backend/static/javascript/build-update-item.js
+++ backend/static/javascript/build-update-item.js
@@ -276,7 +276,7 @@ function getUploadListPict() {
             let val_id = 0;
             for (let i = 0; i < response_size;i++) {
                 if (response_size[i].id > val) {
-                    val_id = response_size[i].id
+                    val_id = serverResponse4[i].id
                     val = i
                 }
             }
