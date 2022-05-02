2cda3da0bb76df20d68127472fddb8f4d7d9b5c6
Jonathan McGaha <87310106+BsMWgEr@users.noreply.github.com>
2022-05-02 15:31:14 -0400

    adding upload functions to create form

--- backend/static/javascript/build-update-item.js
+++ backend/static/javascript/build-update-item.js
@@ -271,7 +271,7 @@ function getUploadListPict() {
             let final_str = ''
             let str_start = ''
             let main_str = ''
-            let last_value = serverResponse4.response[response_size].id
+            let last_value = '2'  //serverResponse4.response[response_size].id
             str_start = '<option value="'+ last_value +'" selected>'+ serverResponse4[response_size].key +'</option>'
             for (let i = 0; i < response_size; i++) {
                 main_str += "<option value='" + serverResponse4.response[i].id + "'>" + serverResponse4.response[i].key + "</option>"
