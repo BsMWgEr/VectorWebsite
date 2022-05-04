2123db21c924d65fc979065ddaa542fef01ff255
Jonathan McGaha <87310106+BsMWgEr@users.noreply.github.com>
2022-05-04 19:33:03 -0400

    adding upload functions to create form

--- backend/static/javascript/build-update-item.js
+++ backend/static/javascript/build-update-item.js
@@ -246,7 +246,7 @@ function createPictureChange() {
     change_display2.innerHTML = ''
     div_change.className = 'div-change'
     document.getElementById('upload-file-div').innerHTML = "<h3 style='color: white;'>Upload A New Picture</h3>"
-        + "<input onchange='fileInputChangedCreate()' type='file' name='file' id='files' multiple='multiple' accept='image/*' />"
+        + "<input type='file' name='file' id='files' multiple='multiple' accept='image/*' />"
     let fileInput = document.getElementById('files')
     fileInput.addEventListener('change', fileInputChangedCreate)
 }
