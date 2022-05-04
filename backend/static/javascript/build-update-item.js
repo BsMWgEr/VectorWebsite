2de31086834341f25199467d43c2c13fda022be1
Jonathan McGaha <87310106+BsMWgEr@users.noreply.github.com>
2022-05-04 15:15:41 -0400

    adding upload functions to create form

--- backend/static/javascript/build-update-item.js
+++ backend/static/javascript/build-update-item.js
@@ -232,9 +232,9 @@ function PictureChange() {
     div_change.className = 'div-change'
     getUploadListPict()
     document.getElementById('upload-file-div').innerHTML = "<h3 style='color: white;'>Upload A New Picture</h3>"
-        + "<input type='file' name='file' id='files' multiple='multiple' accept='image/*' />"
+        + "<input onchange='fileInputChangedUpdate()' type='file' name='file' id='files' multiple='multiple' accept='image/*' />"
     let fileInput = document.getElementById('files')
-    fileInput.addEventListener('change', fileInputChangedUpdate)
+
 
 }
 
@@ -246,9 +246,9 @@ function createPictureChange() {
     change_display2.innerHTML = ''
     div_change.className = 'div-change'
     document.getElementById('upload-file-div').innerHTML = "<h3 style='color: white;'>Upload A New Picture</h3>"
-        + "<input type='file' name='file' id='files' multiple='multiple' accept='image/*' />"
+        + "<input onchange='fileInputChangedCreate()' type='file' name='file' id='files' multiple='multiple' accept='image/*' />"
     let fileInput = document.getElementById('files')
-    fileInput.addEventListener('change', fileInputChangedCreate)
+
 }
 
 
@@ -333,9 +333,9 @@ function ConfirmChange() {
     getUploadListCR()
 
     document.getElementById('upload-file-div').innerHTML = "<h3 style='color: white;'>Upload A New Report</h3>"
-        + "<input type='file' name='file' id='files' multiple='multiple' accept='application/pdf' />"
+        + "<input onchange='fileInputChangedUpdate()' type='file' name='file' id='files' multiple='multiple' accept='application/pdf' />"
     let fileInput = document.getElementById('files')
-    fileInput.addEventListener('change', fileInputChangedUpdate)
+
 }
 
 function createConfirmChange() {
@@ -346,9 +346,9 @@ function createConfirmChange() {
     change_display2.innerHTML = ''
     div_change.className = 'div-change'
     document.getElementById('upload-file-div').innerHTML = "<h3 style='color: white;'>Upload A New Report</h3>"
-        + "<input type='file' name='file' id='files' multiple='multiple' accept='application/pdf' />"
+        + "<input onchange='fileInputChangedCreate()' type='file' name='file' id='files' multiple='multiple' accept='application/pdf' />"
     let fileInput = document.getElementById('files')
-    fileInput.addEventListener('change', fileInputChangedCreate)
+
 }
 
 
