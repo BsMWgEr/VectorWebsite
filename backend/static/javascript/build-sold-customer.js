0de198915b69401af2c597ff9c3533cad4d83d1a
Jonathan McGaha <87310106+BsMWgEr@users.noreply.github.com>
2022-05-10 01:01:55 -0400

    mobile view for frontpage

--- backend/static/javascript/build-sold-customer.js
+++ backend/static/javascript/build-sold-customer.js
@@ -79,6 +79,7 @@ function createSoldDetail() {
         let right_node = document.createTextNode("new text goes here")
         right_div.appendChild(right_node)
         div_box.appendChild(right_div)
+    }
         let obj_id = document.getElementById('e-id').innerHTML
         let xhr = new XMLHttpRequest()
         let method = 'GET'
@@ -91,11 +92,14 @@ function createSoldDetail() {
             console.log(serverResponse.response)
             let str_detail = ''
             for (let i = 0; i < answer.length; i++) {
-                str_detail += '<div id="" class="build-page-sold-detail"><p>' + 'id: ' + answer[i].id + '</p><p>date sold: ' + answer[i].date_sold + '</p><p>info: ' + answer[i].info + '</p><p>other: ' + answer[i].other + '</p><p>created_date' + answer[i].created_date + '</p></div>'
+                str_detail += '<div id="" class="build-page-sold-detail"><p>'
+                    + 'id: ' + answer[i].id + '</p><p>date sold: ' + answer[i].date_sold
+                    + '</p><p>info: ' + answer[i].info + '</p><p>other: ' + answer[i].other
+                    + '</p><p>created_date' + answer[i].created_date + '</p></div>'
             }
 
-            right_div.innerHTML = str_detail
+            document.getElementById('right-sold-bottom-div').innerHTML = str_detail
         }
         xhr.send()
-    }
+
 }
