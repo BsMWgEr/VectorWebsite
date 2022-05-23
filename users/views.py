09da86beb7f7165e28c0887d7984a77290e08b07
Jonathan McGaha <87310106+BsMWgEr@users.noreply.github.com>
2022-05-22 22:52:44 -0400

    Adding customer user pages

--- users/views.py
+++ users/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth import authenticate, login, logout
 from backend.models import SearchQuery
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-
+from backend.models import Customer
 # Create your views here.
 
 
@@ -50,15 +50,20 @@ def registration_view(request):
     # new_user = User.objects.create_user(username=name, email=email, password=password)
     form = UserCreationForm(request.POST or None)
     if form.is_valid():
+        new_customer = Customer()
+        new_customer.first_name = first_name
+        new_customer.last_name = last_name
+        new_customer.email = email
+        new_customer.phone_number = phone_number
+        new_customer.save()
         info = form.data
         print(info)
         u1 = info.get('username')
         p1 = info.get('password1')
         e1 = info.get('email')
-        print(e1)
         obj = User.objects.create_user(username=u1, password=p1, first_name=first_name, last_name=last_name, email=email)
-        print(obj)
         obj.save()
+        login(request, obj)
         return redirect('/users/home/')
     context = {"form": form}
     return render(request, 'register.html', context=context)
