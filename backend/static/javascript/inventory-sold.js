8223ea5cae8dec8403206cb30f3c39b7ee869154
Jonathan McGaha <87310106+BsMWgEr@users.noreply.github.com>
2022-05-29 19:52:08 -0400

    SHIPPING API's woooo Inventory page

--- backend/static/javascript/inventory-sold.js
+++ backend/static/javascript/inventory-sold.js
@@ -1,4 +1,6 @@
 
+
+
 function shippingDataSubmit(event, x){
     event.preventDefault()
     const my_Form = event.target
@@ -41,8 +43,8 @@ function shippingAddressSubmit(event, x){
                         + ' ' + selected_item[0].city + ', ' + selected_item[0].state + ' ' + selected_item[0].zipcode + '</option>'
                 }
                 for (let i = 1; i < answer.length; i++) {
-                    new_str += '<option value="' + answer[i].id + '">Customer ID: ' + answer[i].customer_id
-                    + answer[i].city + ', ' + answer[i].state + answer[i].zipcode + '</option>'
+                    new_str += `<option value="${answer[i].id}">Customer ID: ${answer[i].customer_id}
+                    + ${answer[i].city} , ${answer[i].state} ${answer[i].zipcode}</option>`
                 }
                 document.getElementById('id_shipping_address').innerHTML = new_str
             }
@@ -59,4 +61,27 @@ function openShippingDataForm(x) {
 
 function openShippingAddressForm(x) {
     document.getElementById('shipping_address_form-' + x.toString()).className = 'shipping-address-form'
+}
+
+
+function updateInventorySold() {
+    let new_div = document.createElement('div')
+    new_div.setAttribute('id', 'inventory-sold-update-btn-group')
+    let button1 = document.createElement('button')
+    let button2 = document.createElement('button')
+    let button3 = document.createElement('button')
+    let button4 = document.createElement('button')
+    new_div.style.display = 'flex'
+    button1.innerHTML = `Update Date`
+    button1.className = 'inventory-sold-btn-group'
+    button2.className = 'inventory-sold-btn-group'
+    button2.innerHTML = `Update Info`
+    button3.className = 'inventory-sold-btn-group'
+    button3.innerHTML = `Update Other Info`
+    button4.className = 'inventory-sold-btn-group'
+    button4.innerHTML = `Change Customer`
+    new_div.appendChild(button1 && button2 && button3 && button4)
+    document.querySelectorAll('.inventory-sold-btn-group')
+    document.getElementById('update-sold-information-btn').append(new_div)
+
 }
