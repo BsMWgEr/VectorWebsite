2f32ad94232f352b3c35e71b6c942600a9934986
Jonathan McGaha <87310106+BsMWgEr@users.noreply.github.com>
2022-05-30 02:58:13 -0400

    SHIPPING API's woooo Inventory page

--- backend/static/javascript/inventory-sold.js
+++ backend/static/javascript/inventory-sold.js
@@ -122,7 +122,7 @@ function updateInventorySoldData(event, obj_id) {
             document.getElementById('sold-data-one-' + obj_id).innerHTML = `Sold
             Data: ${answer[0].id} --- Sold on: ${answer[0].date_sold}`
             document.getElementById('inventory-sold-data-id-number-' + obj_id).innerHTML = `Sold Data: 
-            ${answer[0].id} --- Sold on: ${answer[0].sold_date}`
+            ${answer[0].id} --- Sold on: ${answer[0].date_sold}`
             document.getElementById('sold-data-ul').innerHTML = `
                         <li>Purchased By: ${answer[0].purchased_by_id}</li>
                         <li>Date Sold: ${answer[0].date_sold}</li>
