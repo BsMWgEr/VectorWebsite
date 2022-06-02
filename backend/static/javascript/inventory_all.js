0e5d4820faa1054082c17244b8ab7dbde28453a7
Jonathan McGaha <87310106+BsMWgEr@users.noreply.github.com>
2022-06-02 08:55:06 -0400

    manager page title in nav bar white

--- backend/static/javascript/inventory_all.js
+++ backend/static/javascript/inventory_all.js
@@ -1,5 +1,6 @@
 function superFilter(filter_by, x) {
     let current_url = document.location.href
+    let current_url2 = document.location.href
     let new_str = ''
     for (let i = 0; i < x.length; i++) {
         if (x[i] === ' ') {
@@ -9,9 +10,12 @@ function superFilter(filter_by, x) {
     }
     current_url = current_url.split('.')
     current_url = current_url[2].split('/')
+    if (current_url[3] !== '') {
+        current_url2 += '?'
+    } else current_url2 += current_url[3] + '&'
     console.log(x)
     console.log(current_url)
-    let url = `/manager/${current_url[2]}/?filter_by=${new_str}`
+    let url = `${current_url[2]}filter_by=${new_str}`
     console.log(url)
     window.location.assign(url)
     console.log(filter_by, new_str, url)
