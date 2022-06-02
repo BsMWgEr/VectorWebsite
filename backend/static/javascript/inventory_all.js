252075a534f1bfeb19cb236d0d3688a357b891b1
Jonathan McGaha <87310106+BsMWgEr@users.noreply.github.com>
2022-06-02 01:05:27 -0400

    manager page title in nav bar white

--- backend/static/javascript/inventory_all.js
+++ backend/static/javascript/inventory_all.js
@@ -1,3 +1,9 @@
 function superFilter(filter_by, value) {
-    console.log(window.location.href)
+    let current_url = document.location.href
+    current_url = current_url.split('.')
+    current_url = current_url[2].split('/')
+    let xhr = new XMLHttpRequest()
+    xhr.responseType = 'json'
+    xhr.open('GET', `/manager/${current_url[1]}/?filter_by=${value}`, false)
+    xhr.send()
 }
